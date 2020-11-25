90c8c99e65a623f994e1803efb3a0b8ce0d8f4af
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-25 18:05:41 +0200

    connecting redux to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import "./styles/main.scss";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector("#root"));
