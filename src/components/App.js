c1d40d435021e4333cfb1d365f685c09fe0f78d4
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-26 01:12:34 +0200

    remove testing notifications from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setClientWidth } from "../redux/clientWidth/clientWidthAction";
-import { makeAlertNotification, makeSuccessNotification } from '../redux/notifications/notificationOperations';
-import Notification from '../components/Notification/';
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(makeSuccessNotification('lorem lorem lorem'))
-    dispatch(setClientWidth(document.documentElement.clientWidth));
-  }, []);
-  return <Notification/>;
+  return <p>hello</p>;
 }
 
 export default App;
