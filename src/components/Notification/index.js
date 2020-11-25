415622b6bd1604032fdfea706d42554d57e8d702
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-25 23:30:32 +0200

    alert

---
+++ src/components/Notification/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styles from "./Alert.module.scss";
+
+// const Notification = () => (
+//   <CSSTransition
+//     in={text ? true : false}
+//     unmountOnExit
+//     classNames="alert"
+//     timeout={250}
+//   >
+//     <div style={style}>
+//       <p>{text}</p>
+//     </div>
+//   </CSSTransition>
+// );
