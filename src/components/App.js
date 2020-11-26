d52e76e16dfa6742e56c9ee9d4f343f13548a96b
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-26 23:17:39 +0200

    loader

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setClientWidth } from "../redux/clientWidth/clientWidthAction";
 import Notification from "./Notification";
+import Loader from './Loader'
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setClientWidth(document.documentElement.clientWidth));
   }, []);
-  return <Notification />;
+  return <Loader />;
 }
 
 export default App;
