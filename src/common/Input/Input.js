30ca3a9692cbe0bad27c3a5cb95cd21772ced1ba
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-11-26 19:56:07 +0200

    letter-spacing in password

--- src/common/Input/Input.js
+++ src/common/Input/Input.js
@@ -2,10 +2,11 @@ import React from "react";
 import classNames from "classnames";
 import styles from "./Input.module.css";
 
-const Input = ({ type, className, ...props }) => {
+const Input = ({ type, value, className, ...props }) => {
   return (
     <label className={classNames(styles[className])}>
       <input
+        value={value}
         type={type}
         className={classNames(styles.input, styles[type])}
         {...props}
