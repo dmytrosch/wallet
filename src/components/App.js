ad747216a0a2588ed1ce3a317a24ba9de5417461
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-11-27 13:37:10 +0200

    date input and hover buttons

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,27 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setClientWidth } from "../redux/clientWidth/clientWidthAction";
 import Notification from "./Notification";
+import Input from "../common/Input/Input";
+import Button from "../common/Button/Button";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setClientWidth(document.documentElement.clientWidth));
   }, []);
-  return <Notification />;
+  return (
+    <>
+      <Notification />
+      <Input type="number" />
+      <br></br>
+      <Input type="date" />
+      <br></br>
+      <Button color="green">DDD</Button>
+      <br></br>
+      <br></br>
+      <Button>DDD</Button>
+    </>
+  );
 }
 
 export default App;
