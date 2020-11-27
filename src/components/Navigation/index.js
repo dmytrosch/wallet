e0a99618f805bb4f095b1e00fb9b5feb1a0607e5
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-28 00:41:30 +0200

    exact=true in navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React from "react";
 // import { NavLink } from 'react-router-dom';
 
-import home from '../../assets/Home.svg';
-import dollar from '../../assets/Dollar.svg';
-import statistics from '../../assets/Statistics.svg';
-import styles from './Navigation.module.css';
+import home from "../../assets/Home.svg";
+import dollar from "../../assets/Dollar.svg";
+import statistics from "../../assets/Statistics.svg";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
- 
   return (
     <div>
       <ul className={styles.navList}>
         <li key="home" className={styles.navListItem}>
           <a href="/home" className={styles.activeNavLink}>
-              <img src={home} alt="main page" className={styles.navListItemIcon}/>
-              
+            <img
+              src={home}
+              alt="main page"
+              className={styles.navListItemIcon}
+            />
+
             <span className={styles.navListType}>Главная</span>
           </a>
         </li>
         <li key="statistics" className={styles.navListItem}>
           <a href="/statistics" className={styles.activeNavLink}>
-          <img src={statistics} alt="statistics" className={styles.navListItemIcon}/>
+            <img
+              src={statistics}
+              alt="statistics"
+              className={styles.navListItemIcon}
+            />
             <span className={styles.navListType}>Статистика</span>
           </a>
         </li>
         <li key="currency" className={styles.navListItem}>
-          <a href="/currency" exact className={styles.activeNavLink}>
-          <img src={dollar} alt="statistics" className={styles.navListItemIcon}/>
+          <a href="/currency" exact="true" className={styles.activeNavLink}>
+            <img
+              src={dollar}
+              alt="statistics"
+              className={styles.navListItemIcon}
+            />
             <span className={styles.navListType}></span>
           </a>
         </li>
