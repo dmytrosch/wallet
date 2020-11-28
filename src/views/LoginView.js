1613d05b44e0b511bf33cbfa87b33fa7e4998836
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-11-28 13:30:33 +0200

    added icons to inputs

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./LoginView.module.css";
 import Login from "../components/authComponents/LogIn";
+import Input from "../common/Input";
 
 const LoginView = () => {
   return (
     <>
       <div className={styles.loginContainer}>
-        <div className={styles.leftBg}>
+        <Input type="email" className="email" />
+
+        <Input type="text" className="name" />
+
+        <Input type="password" className="password" />
+        {/* <div className={styles.leftBg}>
           <div className={styles.bgContainer}></div>
           <div className={styles.eclipsContainer}>
             <h2 className={styles.appTitle}>Finance App</h2>
@@ -14,7 +20,7 @@ const LoginView = () => {
         </div>
         <div className={styles.rightBg}>
           <div className={styles.pinkEclipsContainer}></div>
-        </div>
+        </div> */}
       </div>
       {/* <Login /> */}
     </>
