dbe3aba4181882256450e1da38803d171193462a
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-11-28 15:24:16 +0200

    nothing

--- src/components/Wallet/Balance/Balance.js
+++ src/components/Wallet/Balance/Balance.js
@@ -1,12 +1,15 @@
 import React from "react";
+import style from "./Balance.module.css";
 import { useSelector } from "react-redux";
 import { getBalance } from "../../../redux/wallet/walletSelectors";
 
 export default function Balance() {
   const balance = useSelector(getBalance);
+  const { container, text, currentbalance } = style;
   return (
-    <div>
-      <p>Ваш баланс {balance}</p>
+    <div className={container}>
+      <p className={text}>Ваш баланс</p>
+      <p className={currentbalance}>₴{balance}</p>
     </div>
   );
 }
