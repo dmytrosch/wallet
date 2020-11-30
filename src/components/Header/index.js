c3b2524da611bfeddf9cadcc63cf05af1fd769c6
elena-salam <elena.salamakha.ua@gmail.com>
2020-11-30 13:48:41 +0200

    added styles to Header and Navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-// import {NavLink} from 'react-router-dom';
 import { userName } from "../../redux/auth/authSelectors";
 import { logout } from "../../redux/auth/authOperations";
-import { ReactComponent as WalletImg } from "../../assets/icons/WalletImg.svg";
 import { ReactComponent as ExitArrow } from "../../assets/icons/Arrow_exit.svg";
 import styles from "./Header.module.css";
 
 const Header = ({ userName, onLogout }) => {
   return (
     <section className={styles.headerContainer}>
-      <span>
-        <WalletImg /> Wallet
-      </span>
-      <ul>
-        <span className={styles.headerUserName}>{userName}</span>
-        <button
-          className={styles.headerButton}
-          type="button"
-          onClick={onLogout}
-        >
-          <ExitArrow />
-          Выйти
-        </button>
-      </ul>
+        <div className={styles.logo}></div>
+        <ul className={styles.ulExit}>
+            <p className={styles.headerUserName}>userName</p>
+            <button className={styles.headerButton} type="button" onClick={onLogout}><ExitArrow/>
+                <span className={styles.headerExitWord}>Выход</span>
+            </button>
+        </ul>
+    
     </section>
   );
 };
