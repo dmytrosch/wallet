80731f11b17fac8cb510ea365cea34580334ac01
DmytroLysenko <DmytroLysenko@users.noreply.github.com>
2020-11-30 21:54:18 +0200

    commit

--- src/components/Wallet/TransactionsTable/index.js
+++ src/components/Wallet/TransactionsTable/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import classNames from "classnames";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import { getAllTransactions } from "../../../redux/wallet/walletSelectors";
 
 import styles from "./TransactionsTable.module.css";
+import animation from "./transTable.animation.module.css";
 
 export default function TransactionsTable() {
   const INCOME = "INCOME";
@@ -25,46 +27,54 @@ export default function TransactionsTable() {
           Баланс
         </div>
       </li>
-      {allTransactions.map((item) => (
-        <li
-          key={item.id}
-          className={item.type === INCOME ? styles.rowGreen : styles.rowRed}
-        >
-          <div className={classNames(styles.item, styles.date)}>
-            <span className={styles.itemTitle}>Дата</span>
-            <span className={styles.itemData}>{item.transactionDate}</span>
-          </div>
-          <div className={classNames(styles.item, styles.type)}>
-            <span className={styles.itemTitle}>Тип</span>
-            <span className={styles.itemData}>
-              {item.type === INCOME ? "+" : "-"}
-            </span>
-          </div>
-          <div className={classNames(styles.item, styles.category)}>
-            <span className={styles.itemTitle}>Категория</span>
-            <span className={styles.itemData}>{item.category}</span>
-          </div>
-          <div className={classNames(styles.item, styles.comment)}>
-            <span className={styles.itemTitle}>Комментарий</span>
-            <span className={styles.itemData}>{item.comment}</span>
-          </div>
-          <div className={classNames(styles.item, styles.amount)}>
-            <span className={styles.itemTitle}>Сумма</span>
-            <span
-              className={classNames(
-                item.type === INCOME ? styles.colorGreen : styles.colorRed,
-                styles.itemData
-              )}
+      <TransitionGroup>
+        {allTransactions.map((item) => (
+          <CSSTransition
+            timeout={250}
+            key={item.id}
+            classNames={animation}
+            unmountOnExit
+          >
+            <li
+              className={item.type === INCOME ? styles.rowGreen : styles.rowRed}
             >
-              {item.amount}
-            </span>
-          </div>
-          <div className={classNames(styles.item, styles.balanceAfter)}>
-            <span className={styles.itemTitle}>Баланс</span>
-            <span className={styles.itemData}>{item.balanceAfter}</span>
-          </div>
-        </li>
-      ))}
+              <div className={classNames(styles.item, styles.date)}>
+                <span className={styles.itemTitle}>Дата</span>
+                <span className={styles.itemData}>{item.transactionDate}</span>
+              </div>
+              <div className={classNames(styles.item, styles.type)}>
+                <span className={styles.itemTitle}>Тип</span>
+                <span className={styles.itemData}>
+                  {item.type === INCOME ? "+" : "-"}
+                </span>
+              </div>
+              <div className={classNames(styles.item, styles.category)}>
+                <span className={styles.itemTitle}>Категория</span>
+                <span className={styles.itemData}>{item.category}</span>
+              </div>
+              <div className={classNames(styles.item, styles.comment)}>
+                <span className={styles.itemTitle}>Комментарий</span>
+                <span className={styles.itemData}>{item.comment}</span>
+              </div>
+              <div className={classNames(styles.item, styles.amount)}>
+                <span className={styles.itemTitle}>Сумма</span>
+                <span
+                  className={classNames(
+                    item.type === INCOME ? styles.colorGreen : styles.colorRed,
+                    styles.itemData
+                  )}
+                >
+                  {item.amount}
+                </span>
+              </div>
+              <div className={classNames(styles.item, styles.balanceAfter)}>
+                <span className={styles.itemTitle}>Баланс</span>
+                <span className={styles.itemData}>{item.balanceAfter}</span>
+              </div>
+            </li>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </ul>
   );
 }
