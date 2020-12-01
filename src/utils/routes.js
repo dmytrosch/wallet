c6cfe4a5fd65f2f7ee4490b7a9195571904ce350
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-12-01 08:37:03 +0200

    working with moving naviagtion

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -8,7 +8,7 @@ export default [
     component: lazy(() =>
       import("../views/MainView" /* webpackChunkName: "home" */)
     ),
-    private: true,
+    private: false,
     restricted: false,
     onlyMobile: false,
   },
@@ -28,7 +28,9 @@ export default [
     label: "Currency",
     exact: true,
     component: lazy(() =>
-      import("../views/CurrencyMobileView/CurrencyMobileView" /* webpackChunkName: "currency" */)
+      import(
+        "../views/CurrencyMobileView/CurrencyMobileView" /* webpackChunkName: "currency" */
+      )
     ),
     private: true,
     restricted: false,
