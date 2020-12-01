b67d0854337fbd69e2361d10c03176eb7d3acb46
Dmytro.Sch <dmytro.sch@list.ru>
2020-12-02 01:46:39 +0200

    render balance after loading

--- src/components/Wallet/Balance/Balance.js
+++ src/components/Wallet/Balance/Balance.js
@@ -4,15 +4,16 @@ import css from "./Balance.module.css";
 import formatNumber from "../../../utils/formatNumber";
 import { useSelector } from "react-redux";
 import { getBalance } from "../../../redux/wallet/walletSelectors";
+import { isLoading } from "../../../redux/loading/loadingSelector";
 const { container, text, currentbalance } = css;
 
 export default function Balance() {
   const balance = useSelector(getBalance);
-
+  const loading = useSelector(isLoading);
   return (
     <div className={container}>
       <p className={text}>ВАШ БАЛАНС</p>
-      <>{formatNumber(balance, currentbalance, "₴ ")}</>
+      <>{!loading && formatNumber(balance, currentbalance, "₴ ")}</>
     </div>
   );
 }
