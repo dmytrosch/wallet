e1b3e343ddbe60f5e8adfd73a4e9d5a92e42dab8
Dmytro.Sch <dmytro.sch@list.ru>
2020-12-02 16:15:13 +0200

    header fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,32 @@
 import React from "react";
-import { connect } from "react-redux";
-import { userName } from "../../redux/auth/authSelectors";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserName } from "../../redux/auth/authSelectors";
 import { logout } from "../../redux/auth/authOperations";
 import { ReactComponent as ExitArrow } from "../../assets/icons/Arrow_exit.svg";
 import styles from "./Header.module.css";
 
-const Header = ({ userName, onLogout }) => {
+const Header = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector(getUserName);
+  const onLogout = () => dispatch(logout());
   return (
     <section className={styles.headerContainer}>
+      <NavLink to="/">
         <div className={styles.logo}></div>
-        <ul className={styles.ulExit}>
-            <p className={styles.headerUserName}>{userName}</p>
-            <button className={styles.headerButton} type="button" onClick={onLogout}><ExitArrow/>
-                <span className={styles.headerExitWord}>Выход</span>
-            </button>
-        </ul>
-    
+      </NavLink>
+      <ul className={styles.ulExit}>
+        <p className={styles.headerUserName}>{userName}</p>
+        <button
+          className={styles.headerButton}
+          type="button"
+          onClick={onLogout}
+        >
+          <ExitArrow />
+          <span className={styles.headerExitWord}>Выход</span>
+        </button>
+      </ul>
     </section>
   );
 };
-
-const mapStateToProps = (state) => ({
-  userName: userName(state),
-});
-const mapDispatchToProps = {
-  onLogout: logout,
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,6 +1,4 @@
 export const isAuthentificated = (state) => (state.auth.token ? true : false);
-export const getUserName = (state) => state.auth.user.name;
-
-export const userName = state => state.auth.user.name;
+export const getUserName = (state) => state.auth.user.username;
 
 
