2e5a10aea217944147c7c5d92b71f0a0d7d0422a
Dmytro.Sch <dmytro.sch@list.ru>
2020-12-02 17:19:25 +0200

    fix nav styles

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import classNames from "classnames";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
- 
   return (
     <div className={styles.navContainer}>
       <ul className={styles.navList}>
         <li key="home" className={styles.navListItem}>
-          <NavLink to="/home" className={[styles.activeNavLink, styles.box].join(' ')}>
-            <div className={[styles.img, styles.home ].join(' ')}/>  
+          <NavLink
+            to="/"
+            exact
+            className={styles.box}
+            activeClassName={styles.activeLink}
+          >
+            <div className={classNames(styles.img, styles.home)} />
             <p className={styles.navListType}>Главная</p>
           </NavLink>
         </li>
         <li key="statistics" className={styles.navListItem}>
-          <NavLink to="/statistics" className={[styles.activeNavLink, styles.box].join(' ')}>
-          <div className={[styles.img, styles.statistics ].join(' ')}/>
-          <p className={styles.navListType}>Статистика</p>
+          <NavLink
+            to="/statistics"
+            className={styles.box}
+            activeClassName={styles.activeLink}
+          >
+            <div className={classNames(styles.img, styles.statistics)} />
+            <p className={styles.navListType}>Статистика</p>
           </NavLink>
         </li>
-        <li key="currency" className={styles.navListItem} >
-          <NavLink to="/currency" exact className={styles.activeNavLink}>
-          <div className={[styles.img, styles.currency].join(' ')}/>
+        <li key="currency" className={styles.navListItem}>
+          <NavLink
+            to="/currency"
+            exact
+            className={styles.box}
+            activeClassName={styles.activeLink}
+          >
+            <div className={classNames(styles.img, styles.currency)}/>
             <span className={styles.navListType}></span>
           </NavLink>
         </li>
