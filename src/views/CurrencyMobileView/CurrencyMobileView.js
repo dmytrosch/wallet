4d0188c49d3dc5b0165f65ca1f0172e3ae57f0cf
Slavik Ohorodnik <teatrgsc@gmail.com>
2020-12-03 12:28:00 +0200

    del css, del import

--- src/views/CurrencyMobileView/CurrencyMobileView.js
+++ src/views/CurrencyMobileView/CurrencyMobileView.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Header from "../../components/Header";
-import Navigation from "../../components/Navigation";
 import CurrencyRates from "../../components/Wallet/CurrencyRates/CurrencyRates";
 import AuthLayout from "../authLayout";
-import css from "./CurrencyMobileView.module.css";
+
 
 export default function CurrencyMobileView() {
   React.useEffect(() => (document.title = "Курс валют || Wallet"), []);
